6f738e38901a1e5632a333870adef38517be418d
Musaabfaozi <mosab.albakry@gmail.com>
2021-08-16 02:15:09 +0300

    list_min_max is not available

---
+++ min_max_finding_algo/main.py
@@ -0,0 +1,16 @@
+import random
+
+def list_max(in_list):
+	pass
+
+def list_min(in_list):
+	pass
+
+if __name__ == "__main__":
+	print("hello!")
+
+	val = random.uniform(0, 1000, 1000)
+
+	print(list_min(val))
+
+	print(list_max(val))
