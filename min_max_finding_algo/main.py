625a9b104b0807a0c8f2d86540ecf692fa3ba9a1
mcr19 <mohammad2015>
2021-08-18 20:36:27 +0300

    Done!

--- min_max_finding_algo/main.py
+++ min_max_finding_algo/main.py
@@ -2,21 +2,29 @@ import numpy as np
 
 
 def list_max(in_list):
+    max = 0
     for i in in_list:
-        for j in in_list[i]:
-            print(i)
+        if i > max:
+            max = i
+            # print(max)
+            # print(i)
+    return max
 
 
 def list_min(in_list):
-    pass
+    min = 0
+    for i in in_list:
+        if i < min:
+            min = i
+            # print(max)
+            # print(i)
+    return min
 
 
 if __name__ == "__main__":
-    print("hello!")
+    # print("hello!")
 
     val = np.random.normal(15, 7, 1000).astype(int)
-
-    print(val)
 
     print(list_min(val))
 
