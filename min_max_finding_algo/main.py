7753e2e742d03c3bf06aebbcebe68d93838c1047
mcr19 <mohammad2015>
2021-08-19 19:09:54 +0300

    Error numpy ndarray!

--- min_max_finding_algo/main.py
+++ min_max_finding_algo/main.py
@@ -10,6 +10,7 @@ def list_max(in_list):
             # print(i)
     return imax
 
+
 def list_min(in_list):
     imin = in_list[0]
     for i in in_list:
@@ -19,14 +20,27 @@ def list_min(in_list):
             # print(i)
     return imin
 
+
 def list_sort(in_list):
-	pass
+    in_list_sorted = []
+    isort = in_list[0]
+    for i in in_list:
+        if i > isort:
+            isort = i
+            in_list_sorted.append(i)
+            in_list.remove()  # Error 'numpy.ndarray' object has no attribute 'remove' when i use remove function
+    return in_list_sorted
+
 
 if __name__ == "__main__":
     # print("hello!")
 
-    val = np.random.normal(-100, 2, 1000).astype(int)
+    val = np.random.normal(100, 2, 1000).astype(int)
+
+    print(val)
 
     print(list_min(val), min(val))
 
     print(list_max(val), max(val))
+
+    print(list_sort(val))
