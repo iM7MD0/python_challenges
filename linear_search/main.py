460be569d7a9ae93c677319a0e801908a0e892da
mcr19 <mohammad2015>
2021-08-24 12:07:44 +0300

    Complete Search Challenges!

--- linear_search/main.py
+++ linear_search/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 def search(in_list, val):
@@ -9,17 +10,34 @@ def search(in_list, val):
 
 
 def search_sorted(in_list, val):
-    pass
+    py_list = list(in_list.copy())
+    first_element = py_list[0]
+    middle_element = py_list[int(len(py_list) / 2)]
+    final_element = py_list[-1]
+    for i in py_list:
+        if first_element <= i < middle_element:
+            if i == val:
+                return val
+        elif middle_element <= i <= final_element:
+            if i == val:
+                return val
+    return None
 
 
 if __name__ == '__main__':
-    search_list = np.random.normal(50, 30, 100).astype(int)
+    search_list = np.random.normal(5250, 300, 100000).astype(int)
 
     sorted_search_list = search_list.copy()
     sorted_search_list.sort()
 
     print(search_list)
-
-    print(search(search_list, 44))
-
+    t1 = time.time()
+    print(search_list, 44)
+    t2 = time.time()
+    print("normal search timing =", t2 - t1)
     print(sorted_search_list)
+
+    tp1 = time.time()
+    print(search_sorted(sorted_search_list, 4343))
+    tp2 = time.time()
+    print("sorted timing =", tp2 - tp1)
