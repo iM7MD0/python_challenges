249242b35f8e4b6fac9549c3de450ca8752da457
mcr19 <mohammad2015>
2021-08-29 13:19:34 +0300

    Complete Search Challenges!

--- linear_search/main.py
+++ linear_search/main.py
@@ -11,40 +11,62 @@ def search(in_list, val):
 
 def search_sorted(in_list, val):
     py_list = list(in_list.copy())
-    first_element = py_list[0]
-    middle_element = py_list[int(len(py_list) / 2)]
+    middle_element = int(len(py_list) / 2)
     print("middle_element", middle_element)
     print("py_list[:middle_element]", py_list[:middle_element])
-    final_element = py_list[-1]
-    if first_element <= val < middle_element:
+    print("py_list[middle_element:]", py_list[middle_element:])
+    if val <= middle_element:
         for i in py_list[:middle_element]:
+            print(i)
             if i == val:
                 return val
-    elif middle_element <= val <= final_element:
+    else:
         for j in py_list[middle_element:]:
+            print(j)
             if j == val:
                 return val
     return None
 
 
+def binary_search(in_list, val):
+    py_list = list(in_list.copy())
+    first_element = 0
+    last_element = int(len(py_list) - 1)
+    found = False
+    print(first_element, last_element)
+
+    while first_element <= last_element and not found:
+        middle_element = (first_element + last_element) // 2
+        print(first_element, middle_element, last_element)
+        if py_list[middle_element] == val:
+            found = True
+        else:
+            if val < py_list[middle_element]:
+                last_element = middle_element - 1
+            else:
+                first_element = middle_element + 1
+
+    return found
+
+
 if __name__ == '__main__':
-    
-    #search_list = np.random.normal(52, 30, 100).astype(int)
+    # search_list = np.random.normal(50, 30, 100).astype(int)
     search_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 100, 111, 121, 131, 141, 151, 161, 171, 181]
 
     sorted_search_list = search_list.copy()
     sorted_search_list.sort()
 
-    print(search_list)
-    tm1 = time.time()
-    print(search(search_list, 450))
-    tm2 = time.time()
-    print("normal search timing =", tm2 - tm1)
     print(sorted_search_list)
-
-    tp1 = time.time()
-
-    print(search_sorted(sorted_search_list, 151))
-
-    tp2 = time.time()
-    print("sorted timing =", tp2 - tp1)
+    # tm1 = time.time()
+    # print(search(search_list, 450))
+    # tm2 = time.time()
+    # print("normal search timing =", tm2 - tm1)
+    # print(sorted_search_list)
+    #
+    # tp1 = time.time()
+    #
+    # print(search_sorted(sorted_search_list, 550))
+    #
+    # tp2 = time.time()
+    # print("sorted timing =", tp2 - tp1)
+    print(binary_search(sorted_search_list, ))
--- palindrome/main.py
+++ palindrome/main.py
@@ -1,24 +1,27 @@
+def is_palindrome(phrase: str):
+    global reverse_phrase
+    reverse_phrase = phrase[::-1]
+    print(phrase, reverse_phrase )
+    if phrase.casefold() == reverse_phrase.casefold():
+        return True
+    else:
+        print("Not Palindrome")
+        return False
 
 
-def is_palindrome(phrase : str):
-	pass
-
-
-def palindromify(phrase : str):
-	pass
+def palindromify(phrase: str):
+    pass
 
 
 if __name__ == '__main__':
+    phr_1 = 'lololololol'
+    phr_2 = 'lololololololo'
+    phr_3 = 'Able was I ere I saw Elba'
+    phr_4 = 'A Santa at NASA'
+    phr_5 = 'A nut is a jar of tuna'
 
-	phr_1 = 'lololololol'
-	phr_2 = 'lololololololo'
-	phr_3 = 'Able was I ere I saw Elba'
-	phr_4 = 'A Santa at NASA'
-	phr_5 = 'A nut is a jar of tuna'
-
-
-	assert(is_palindrome(phr_1))
-	assert(not is_palindrome(phr_2))
-	assert(is_palindrome(phr_3))
-	assert(is_palindrome(phr_4))
-	assert(not is_palindrome(phr_5))
+    assert(is_palindrome(phr_1))
+    assert (not is_palindrome(phr_2))
+    assert (is_palindrome(phr_3))
+    assert (is_palindrome(phr_4))
+    assert (not is_palindrome(phr_5))
