d43e8489b57b7fd93774dca073ccaa9ee99cd572
mcr19 <mohammad2015>
2021-08-29 22:16:29 +0300

    Complete PALINDROME CHALLENGE

--- palindrome/main.py
+++ palindrome/main.py
@@ -1,18 +1,17 @@
 def is_palindrome(phrase: str):
     global reverse_phrase
     reverse_phrase = phrase[::-1]
-    # print(phrase, reverse_phrase)
     if phrase.casefold() == reverse_phrase.casefold():
         return True
     else:
-        print("Not Palindrome")
         return False
 
 
 def palindromify(phrase: str):
     if not is_palindrome(phrase):
         palindromable = phrase.casefold() + reverse_phrase.casefold()
-        print(palindromable)
+        return palindromable
+    return phrase
 
 
 if __name__ == '__main__':
@@ -22,9 +21,14 @@ if __name__ == '__main__':
     phr_4 = 'A Santa at NASA'
     phr_5 = 'A nut is a jar of tuna'
 
-    assert(is_palindrome(phr_1))
+    assert (is_palindrome(phr_1))
     assert (not is_palindrome(phr_2))
     assert (is_palindrome(phr_3))
     # assert (is_palindrome(phr_4))
     assert (not is_palindrome(phr_5))
-    palindromify(phr_4)
+
+    print(palindromify(phr_1))
+    print(palindromify(phr_2))
+    print(palindromify(phr_3))
+    print(palindromify(phr_4))
+    print(palindromify(phr_5))
