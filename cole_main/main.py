f72e9037e61b5e1271152cc436ecf7971f61419d
mcr19 <mohammad2015>
2021-08-30 23:11:34 +0300

    New challenges cole main

--- cole_main/main.py
+++ cole_main/main.py
@@ -1,4 +1,19 @@
 import numpy as np
 
-random_matrix = np.random.randint(100, size=(4, 4))
-print(random_matrix)
+
+def start_function(matrix):
+    global max
+    for i in matrix[:, 8]:
+        if i > max:
+            max = i
+            print(i)
+
+
+def find_best_way():
+    pass
+
+
+if __name__ == '__main__':
+    random_matrix = np.random.randint(10, size=(9, 9))
+    print(random_matrix)
+    print(start_function(random_matrix))
