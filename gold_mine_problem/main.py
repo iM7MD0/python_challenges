5ca2325886e5677422a7850909f1ff53ff988764
Musaabfaozi <mosab.albakry@gmail.com>
2021-09-04 15:23:32 +0300

    added greedy attempt

--- gold_mine_problem/main.py
+++ gold_mine_problem/main.py
@@ -2,12 +2,28 @@ import numpy as np
 
 
 def start_function(matrix):
-    global x
-    for i in matrix[:, 8]:
-        if i > x:
-            x = i
-            print(i)
+    path = []
+    first_max = np.argmax(matrix[:, 0])
+    path.append(first_max)
 
+    for i in range(1, len(matrix[0])):
+        current = path[i - 1]
+        if current == 0:
+            possible_vals = matrix[current: current + 2, i]
+            new_move = np.argmax(possible_vals)
+            path.append(current + new_move)
+
+        elif current == len(matrix[:, 0]) - 1:
+            possible_vals = matrix[current - 1: current + 1, i]
+            new_move = np.argmax(possible_vals)
+            path.append(current + new_move - 1)
+
+        else:
+            possible_vals = matrix[current - 1: current + 2, i]
+            new_move = np.argmax(possible_vals)
+            path.append(current + new_move - 1)
+
+    return path
 
 def find_best_way():
     pass
