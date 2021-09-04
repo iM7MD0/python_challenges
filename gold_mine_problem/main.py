b9002e8839c720c7b484cb9ff4516e1d450e2f7e
mcr19 <mohammad2015>
2021-09-04 13:37:59 +0300

    Complete golden mine challenge!

--- gold_mine_problem/main.py
+++ gold_mine_problem/main.py
@@ -2,11 +2,21 @@ import numpy as np
 
 
 def start_function(matrix):
-    global x
-    for i in matrix[:, 8]:
-        if i > x:
-            x = i
-            print(i)
+    path = []
+    first_max = np.argmax(matrix[:, 0])
+    path.append(first_max)
+
+    for i in range(1, len(matrix[0])):
+        here = path[i - 1]
+        if here == 0:
+            values = matrix[here: here + 2, i]
+            next_step = np.argmax(values)
+            path.append(here + next_step)
+        else:
+            values = matrix[here - 1: here + 2, i]
+            next_step = np.argmax(values)
+            path.append(here + next_step - 1)
+    return path
 
 
 def find_best_way():
